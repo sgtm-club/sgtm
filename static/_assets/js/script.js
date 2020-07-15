ab13cadae964c3c86edf55190459f48064db1de9
Manfred Touron <94029+moul@users.noreply.github.com>
2020-07-15 02:33:40 +0200

    fix: race condition in 'on submit -> disable button' on some browsers h/t @falafel

--- static/_assets/js/script.js
+++ static/_assets/js/script.js
@@ -1,5 +1,7 @@
 $('button[type="submit"]').on("click", function() {
-  $(this).prop("disabled", true);
+  setTimeout(function() {
+    $(this).prop("disabled", true);
+  });
 });
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
