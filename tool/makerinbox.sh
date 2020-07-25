7f0e8a1a512ea03a379f4d55025d71bc23216601
Manfred Touron <94029+moul@users.noreply.github.com>
2020-07-25 02:40:00 +0200

    chore: update makerinbox.sh script

--- tool/makerinbox.sh
+++ tool/makerinbox.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 url="$1"
-
-rm -f /tmp/sgtm-track.jpg
+local_artwork=/tmp/sgtm-track-$(date +%s).jpg
 artwork=$(curl "$url?format=json" | jq -r .artworkUrl | sed s/-large.jpg/-t500x500.jpg/)
-wget -q  -O /tmp/sgtm-track.jpg "$artwork"
-ls -la /tmp/sgtm-track.jpg
-set -x
-makerinbox done --attach=/tmp/sgtm-track.jpg "🎶 daily music production $url #ultreme"
-
+wget -q -O "$local_artwork" "$artwork"
+ls -la $local_artwork
+echo ""
+(
+    set -x
+    makerinbox done --attach "$local_artwork" "🎶 daily music production $url #ultreme"
+)
+echo ""
